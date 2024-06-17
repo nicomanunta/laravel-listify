da02484f3719f33d2724688667005d8665d31ad9
nicomanunta <nic.manun@gmail.com>
2024-06-17 16:09:18 +0200

    welcome completata

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-4">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+<div class="container pt-5">
+    <div class="row justify-content-center pt-5">
+        <div class="col-md-8 pt-5">
+            <div class="card mt-5">
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="content pt-5 bg-color-purple color-grey">
+<div class="content pt-5 bg-color-purple color-grey overflow-hidden">
     <div class="container h-100 ">
         <div class="row ">
             <div class="col-12 ">
@@ -9,60 +9,74 @@
                 <h2 class="mt-3 mb-5 welcome-subtitle text-center font-archivo">Dai un ordine alle tue attività!</h2>
                 
             </div>
-        </div>  
-        <div class="col-6 text-center mt-3 height-collapse align-content-center">
-            
-            <button class=" bg-color-purple border-0 mb-4" type="button" data-bs-toggle="collapse" data-bs-target="#loginForm" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="font-archivo font-login color-grey ">Accedi <i class="fa-solid fa-caret-down"></i></span>
-            </button>
-        
-            <div class="collapse navbar-collapse mt-2" id="loginForm">
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-
-                    <div class="mb-4 row">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                        <div class="col-md-6">
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                            @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="mb-4 row">
-                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                        <div class="col-md-6">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                    </div>
-                    <span class="pt-4">Se non hai ancora un profilo, <a class="text-decoration-none text-uppercase font-register font-archivo " href="{{ route('register') }}">{{ __('Registrati') }}</a></span>
-
-                    <div class="my-4 row x">
-                        <div class="text-center">
-                            <button type="submit" class="btn btn-listify-grey font-archivo color-purple">
-                                {{ __('Accedi') }}
-                            </button>
-                        
-                        </div>
-                    </div>
-                    
-                </form>
-            </div>
-        
+            <div class="col-6 text-center mt-3 height-collapse align-content-center">
                 
-        </div>
+                
+                <div class="mb-3 font-archivo font-login color-grey ">ACCEDI </div>
+               
+            
+                <div class="" id="">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+    
+                        <div class="mb-4 row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right font-archivo">{{ __('E-Mail ') }}</label>
+    
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control shadow @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+    
+                        <div class="mb-4 row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right font-archivo">{{ __('Password') }}</label>
+    
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control shadow @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <span class="mt-4 ">Se non hai ancora un profilo, <a class="text-decoration-none text-uppercase  " href="{{ route('register') }}"><span class="font-register font-archivo shadow">{{ __('Registrati!') }}</span></a></span>
+    
+                        <div class="my-4 row x">
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-listify-grey font-archivo color-purple">
+                                    {{ __('Accedi') }}
+                                </button>
+                            
+                            </div>
+                        </div>
+                        
+                    </form>
+                </div>
+            
+                    
+            </div>
+            <div class="col-6 mt-3 position-relative">
+                <i class="position-absolute fa-solid fa-list"></i>
+                <i class="position-absolute fa-solid fa-list-ol"></i>
+                <i class="position-absolute fa-solid fa-list-ul"></i>
+                <i class="position-absolute fa-solid fa-list-check"></i>
+                <i class="position-absolute fa-solid fa-check"></i>
+                <i class="position-absolute fa-regular fa-circle-check"></i>
+                <i class="position-absolute fa-solid fa-x"></i>
+                <i class="position-absolute fa-regular fa-circle-xmark"></i>
+                <i class="position-absolute fa-solid fa-pencil pencil-2"></i>
+                <i class="position-absolute fa-regular fa-pen-to-square"></i>
+                <i class="position-absolute fa-solid fa-file-pen"></i>
+                <i class="position-absolute fa-solid fa-pencil pencil-1"></i>
+            </div>
+        </div>  
         
         
     </div>
